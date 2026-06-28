f3c84699c26655aabbc2e8da335b71d770979b45
Thisuni Weerakoon <kalpanathisuni49@gmail.com>
2026-06-28 17:53:39 +0530

    Added order and low stock material models

--- CraftConnectDashboard/DashboardData.cs
+++ CraftConnectDashboard/DashboardData.cs
@@ -15,11 +15,21 @@ namespace CraftConnect.Models
 
     public class Order
     {
+        public string OrderId { get; set; } = "";
 
+        public string Customer { get; set; } = "";
+
+        public string Status { get; set; } = "";
+
+        public decimal Total { get; set; }
     }
 
     public class LowStockMaterial
     {
+        public string Name { get; set; } = "";
 
+        public string Supplier { get; set; } = "";
+
+        public string Quantity { get; set; } = "";
     }
 }
--- CraftConnectDashboard/obj/Release/net10.0/CraftConnectDashboard.AssemblyInfo.cs
+++ CraftConnectDashboard/obj/Release/net10.0/CraftConnectDashboard.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("CraftConnectDashboard")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3dfa72bbaea2f5d6cd228f0f862ee950c42cafb2")]
 [assembly: System.Reflection.AssemblyProductAttribute("CraftConnectDashboard")]
